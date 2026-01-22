64057a8c231dd272794cd913759fcc75ed563539
KimNhan97 <btknhan090705@gmail.com>
2026-01-22 21:24:54 +0700

    Lap 1- bài 2 - Tính chu vi, diện tích , cạnh nhỏ

---
+++ Lab1_Bai2_ChuviDT/src/TinhChuViDienTich.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class TinhChuViDienTich {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhập chiều dài: ");
+        double dai = sc.nextDouble();
+
+        System.out.print("Nhập chiều rộng: ");
+        double rong = sc.nextDouble();
+
+        double chuVi = 2 * (dai + rong);
+        double dienTich = dai * rong;
+        double canhNho = Math.min(dai, rong);
+
+        System.out.println("Chu vi = " + chuVi);
+        System.out.println("Diện tích = " + dienTich);
+        System.out.println("Cạnh nhỏ = " + canhNho);
+
+        sc.close();
+    }
+}
