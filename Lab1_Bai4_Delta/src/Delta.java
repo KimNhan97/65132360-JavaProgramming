f5d15207e1cf42902d02f4aa37b25ac3b1023074
KimNhan97 <btknhan090705@gmail.com>
2026-01-23 19:11:28 +0700

    lap 1 - bài 4 - chương trình nhập các hệ số của phương trình bậc2.Tính delta và căn delta

---
+++ Lab1_Bai4_Delta/src/Delta.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Delta {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Nhập a: ");
+		double a = sc.nextDouble();
+		
+		System.out.println("Nhập b: ");
+		double b = sc.nextDouble();
+		
+		System.out.println("Nhập c: ");
+		double c = sc.nextDouble();
+		
+		double delta = b*b - 4*a*c;
+		System.out.println("Delta = "+ delta);
+		
+		if(delta >= 0 ) {
+			double canDelta = Math.sqrt(delta);
+			System.out.println("Căn delta = "+ canDelta);
+		}
+		else {
+			System.out.println("Vì delta nhỏ hơn không nên không có căn");
+		}
+		
+		sc.close();
+		
+	}
+
+}
